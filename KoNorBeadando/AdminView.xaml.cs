a4e99db8ff1118d9e5a4fb7985f59ae750ccbed6
Konorka <konor@hotmail.hu>
2018-01-27 15:50:04 +0100

    first commit

---
+++ KoNorBeadando/AdminView.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KoNorBeadando
+{
+    /// <summary>
+    /// Interaction logic for AdminView.xaml
+    /// </summary>
+    public partial class AdminView : Window
+    {
+        eDiaryModelDB context = new eDiaryModelDB();
+        public AdminView()
+        {
+            InitializeComponent();
+           // adminStudentsDataGrid.ItemsSource = context.Student;
+                
+        }
+
+        private void teachersGridButton(object sender, RoutedEventArgs e)
+        {
+            if (adminTeachersDataGrid.IsVisible == false)
+            {
+                adminTeachersDataGrid.Visibility = Visibility.Visible;
+            }
+            adminTeachersDataGrid.DataContext = context.Teacher;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (adminStudentsDataGrid.IsVisible == false)
+            {
+                adminStudentsDataGrid.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            KoNorBeadando.eDiaryDataSet eDiaryDataSet = ((KoNorBeadando.eDiaryDataSet)(this.FindResource("eDiaryDataSet")));
+            // Load data into the table Student. You can modify this code as needed.
+            KoNorBeadando.eDiaryDataSetTableAdapters.StudentTableAdapter eDiaryDataSetStudentTableAdapter = new KoNorBeadando.eDiaryDataSetTableAdapters.StudentTableAdapter();
+            eDiaryDataSetStudentTableAdapter.Fill(eDiaryDataSet.Student);
+            System.Windows.Data.CollectionViewSource studentViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("studentViewSource")));
+            studentViewSource.View.MoveCurrentToFirst();
+        }
+
+        private void SaveBtnClick(object sender, RoutedEventArgs e)
+        {
+            context.SaveChanges();
+        }
+    }
+}
---
+++ KoNorBeadando/LoginView.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KoNorBeadando
+{
+    /// <summary>
+    /// Interaction logic for LoginView.xaml
+    /// </summary>
+    public partial class LoginView : Window
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginClick(object sender, RoutedEventArgs e)
+        {
+            var username = usernameTextBox.Text;
+            var password = passwordTextbox.Password;
+            eDiaryModelDB context = new eDiaryModelDB();
+            //using (eDiaryModelDB context = new eDiaryModelDB())
+            {
+                var user = context.User.FirstOrDefault(x => x.username == username);
+                if (user !=null)
+                {
+                    if (user.password == password)
+                    {
+                        switch (user.user_access_id)
+                        {
+                            case 1:
+                                AdminView adminview = new AdminView();
+                                adminview.Show();
+                                this.Close();break;
+
+                            case 2:
+                                MainWindow teacherView = new MainWindow();
+                                teacherView.Show();
+                                this.Close(); break;
+
+                            case 3:
+                                MainWindow studentView = new MainWindow();
+                                studentView.Show();
+                                this.Close(); break;
+
+                            default: MessageBox.Show("ERROR 404");break;
+                        }
+                        
+                    }
+                    else
+                        MessageBox.Show("Rossz felhasználónév vagy jelszó");
+
+                }else
+                    MessageBox.Show("shit");
+            }
+
+        }
+    }
+}
